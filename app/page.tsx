cb3535689e64ab866054afc36455bf20a9aa7144
vkaiv <ville.kaivola@gmail.com>
2023-10-26 21:43:44 +0300

    removed pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import { Page, Station } from "@/lib/dev-academy-assignment";
+import { Station } from "@/lib/dev-academy-assignment";
 import dynamic from "next/dynamic";
 
 const StationMap = dynamic(() => import("@/app/components/StationMap/StationMap"), { ssr: false });
 
 const Home = async () => {
     const res = await fetch("http://localhost:3000/api/stations");
-    const stations: Page<Station> = await res.json();
+    const stations: Station[] = await res.json();
 
     if (!stations) return null;
     return (
         <div className="h-full flex">
             <div>
-                {stations.content.map((c) => {
+                {stations.map((c) => {
                     return <div key={c.id}>{c.stationName}</div>;
                 })}
             </div>
-            <StationMap />
+            <StationMap stations={stations} />
         </div>
     );
 };
