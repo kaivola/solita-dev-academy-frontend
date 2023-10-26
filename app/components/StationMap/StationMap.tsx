0d26204755cf378ae850cbcc5567d8df041465fc
vkaiv <ville.kaivola@gmail.com>
2023-10-26 17:03:47 +0300

    changed map tiles to Stadia OSM Bright

--- app/components/StationMap/StationMap.tsx
+++ app/components/StationMap/StationMap.tsx
@@ -1,20 +1,15 @@
 "use client";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const StationMap = () => {
     return (
         <div className="h-full w-full">
-            <MapContainer className="h-full" center={[60.1674, 24.944]} zoom={14} maxZoom={12}>
+            <MapContainer className="h-full" center={[60.1674, 24.944]} zoom={14}>
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
                 />
-                <Marker position={[60.1674, 24.944]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
             </MapContainer>
         </div>
     );
