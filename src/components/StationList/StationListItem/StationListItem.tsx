6d5621372d2371ea9af95a21e89b36175b647bfd
vkaiv <ville.kaivola@gmail.com>
2023-10-30 17:56:04 +0200

    clicking on StationListItem updates searchparams

--- src/components/StationList/StationListItem/StationListItem.tsx
+++ src/components/StationList/StationListItem/StationListItem.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
 
 type Props = {
     id: number;
@@ -8,15 +11,21 @@ type Props = {
 };
 
 export const StationListItem = ({ id, name }: Props) => {
+    const router = useRouter();
+
+    const handleClick = (id: number) => {
+        router.push("/" + "?station=" + id);
+    };
+
     return (
         <div className="px-8 border-b flex items-center justify-between hover:bg-slate-100 hover:text-emerald-300">
-            <Link
-                href={`/stations/${id}`}
-                className="rounded-full flex items-center justify-between w-full h-12"
+            <div
+                onClick={() => handleClick(id)}
+                className="rounded-full flex items-center justify-between w-full h-12 hover:cursor-pointer"
             >
                 <div className="text-black">{name}</div>
                 <FontAwesomeIcon icon={faChevronRight} />
-            </Link>
+            </div>
         </div>
     );
 };
