8cd3ee656cc1f755e21fbca0fdb0930a70b85525
vkaiv <ville.kaivola@gmail.com>
2023-11-01 18:53:54 +0200

    error handling

--- src/app/api/stations/[id]/route.ts
+++ src/app/api/stations/[id]/route.ts
@@ -6,10 +6,14 @@ export const BASE_URL = process.env.NEXT_BACKEND_URL;
 
 export const GET = async (_request: Request, { params }: { params: { id: number } }) => {
     const { id } = params;
-    if (!id || isNaN(id))
+    if (!id || isNaN(id)) {
         return NextResponse.json({ error: "Invalid station id", timestamp: new Date() }, { status: 400 });
+    }
 
     const res = await fetch(`${BASE_URL}/stations/${id}`);
+    if (!res.ok) {
+        return NextResponse.json(res.json(), { status: res.status });
+    }
     const data: Station = await res.json();
 
     return NextResponse.json(data);
