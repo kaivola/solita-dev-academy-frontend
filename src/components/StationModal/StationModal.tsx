fe292b28d2f03de1f9839045942a6eb522014485
vkaiv <ville.kaivola@gmail.com>
2023-11-01 19:26:47 +0200

    implemented station info fetching

--- src/components/StationModal/StationModal.tsx
+++ src/components/StationModal/StationModal.tsx
@@ -1,17 +1,40 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import Modal from "@/components/Modal";
+import { Station } from "@/lib/dev-academy-assignment";
 
 export const StationModal = () => {
+    const router = useRouter();
+    const [station, setStation] = useState<Station>();
     const params = useSearchParams();
     const stationId = params.get("station");
 
-    if (!stationId) return null;
+    const closeModal = () => {
+        setStation(undefined);
+        router.back();
+    };
+
+    useEffect(() => {
+        if (!stationId) return;
+        (async () => {
+            const res = await fetch(`http://localhost:3000/api/stations/${stationId}`);
+            if (res.ok && res.status == 200) {
+                setStation(await res.json());
+            }
+        })();
+    }, [stationId]);
+
+    if (!station || !stationId) return null;
     return (
-        <Modal>
-            <div className="bg-white h-96 w-96">hello</div>
+        <Modal handleClose={closeModal}>
+            <div className="bg-slate-50 w-2/5 p-8 rounded-2xl shadow-xl mb-16">
+                <div className="text-2xl text-slate-800">{station.stationName}</div>
+                <div className="text-lg text-slate-800">{station.stationAddress}</div>
+            </div>
         </Modal>
     );
 };
