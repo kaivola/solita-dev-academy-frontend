14a83a7aa68f6fe8689fc9f9f3e189d169af0628
vkaiv <ville.kaivola@gmail.com>
2023-11-02 19:19:24 +0200

    showing station statistics using InfoCards

--- src/components/StationModal/StationModal.tsx
+++ src/components/StationModal/StationModal.tsx
@@ -4,12 +4,14 @@ import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import InfoCard from "@/components/InfoCard";
 import Modal from "@/components/Modal";
-import { Station } from "@/lib/dev-academy-assignment";
+import { StationDto } from "@/lib/dev-academy-assignment";
+import { formatDistance, formatDuration } from "@/lib/formatStationStatistics";
 
 export const StationModal = () => {
     const router = useRouter();
-    const [station, setStation] = useState<Station>();
+    const [station, setStation] = useState<StationDto>();
     const params = useSearchParams();
     const stationId = params.get("station");
 
@@ -31,9 +33,21 @@ export const StationModal = () => {
     if (!station || !stationId) return null;
     return (
         <Modal handleClose={closeModal}>
-            <div className="bg-slate-50 w-2/5 p-8 rounded-2xl shadow-xl mb-16">
-                <div className="text-2xl text-slate-800">{station.stationName}</div>
-                <div className="text-lg text-slate-800">{station.stationAddress}</div>
+            <div className="xl:w-1/2 mb-16 bg-white py-8 px-12 rounded-2xl shadow-xl">
+                <div className="text-3xl">{station.name}</div>
+                <div className="text-lg">{station.address}</div>
+                <div className="mt-4 grid lg:grid-cols-2 gap-4">
+                    <InfoCard title={"Asemalta alkaneet matkat"} value={station.numOfJourneysStarting} />
+                    <InfoCard title={"Asemalle päättyneet matkat"} value={station.numOfJourneysEnding} />
+                    <InfoCard
+                        title={"Matkojen keskipituus"}
+                        value={formatDistance(station.averageDistanceOfJourneys)}
+                    />
+                    <InfoCard
+                        title={"Matkojen keskimääräinen kesto"}
+                        value={formatDuration(station.averageDurationOfJourneys)}
+                    />
+                </div>
             </div>
         </Modal>
     );
