cb47acbb0348aaf3b121a1c1ad794cb46b79833d
vkaiv <ville.kaivola@gmail.com>
2023-11-04 14:31:37 +0200

    added Dropdown component

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,50 @@
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+type Props = {
+    onSelect: (option: any) => void;
+    options: string[];
+    title: string;
+};
+
+export const Dropdown = ({ onSelect, options, title }: Props) => {
+    const [selected, setSelected] = useState<string | undefined>();
+    const [open, setOpen] = useState<boolean>(false);
+
+    const handleSelect = (option?: string) => {
+        onSelect(option);
+        setSelected(option);
+        setOpen(false);
+    };
+
+    return (
+        <div
+            className="flex justify-between items-center hover:cursor-pointer"
+            onClick={() => setOpen(!open)}
+        >
+            <div className="w-full">{selected ?? title}</div>
+            <div className="relative flex">
+                <button className="z-10 block p-2">
+                    <FontAwesomeIcon icon={faChevronDown} style={{ fontSize: 16 }} />
+                </button>
+
+                {open && (
+                    <div className="absolute top-4 right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+                        {options.map((option) => {
+                            return (
+                                <div
+                                    key={option}
+                                    onClick={() => handleSelect(option)}
+                                    className="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-slate-100"
+                                >
+                                    {option}
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
