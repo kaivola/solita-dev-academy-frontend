0a5cef3dcecacc7f9fe6f7e46a000e8ecb34afdb
vkaiv <ville.kaivola@gmail.com>
2023-11-04 17:33:59 +0200

    added button for clearing search

--- src/components/StationList/StationSearch/StationSearch.tsx
+++ src/components/StationList/StationSearch/StationSearch.tsx
@@ -1,4 +1,6 @@
-import { Dispatch, SetStateAction } from "react";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 import { Station } from "@/lib/dev-academy-assignment";
 
@@ -8,21 +10,38 @@ type Props = {
 };
 
 export const StationSearch = ({ stations, setStations }: Props) => {
-    const handleInput = (val: String) => {
+    const [query, setQuery] = useState<string>("");
+
+    useEffect(() => {
         setStations(
             stations.filter(
                 (station) =>
-                    station.stationName.toLowerCase().includes(val.toLowerCase()) ||
-                    station.stationAddress.toLowerCase().includes(val.toLowerCase())
+                    station.stationName.toLowerCase().includes(query.toLowerCase()) ||
+                    station.stationAddress.toLowerCase().includes(query.toLowerCase())
             )
         );
-    };
+    }, [query, setStations, stations]);
+
+    const handleChange = (val: string) => setQuery(val);
+
+    const clearSearch = () => setQuery("");
 
     return (
-        <input
-            className="bg-gray-100 h-10 m-2 rounded-md pl-2"
-            placeholder="Hae asemaa"
-            onChange={(event) => handleInput(event.target.value)}
-        />
+        <div className="flex">
+            <input
+                className="border-y-2 border-l-2 h-11 ml-6 my-2 pl-2 rounded-l-md focus:outline-none"
+                placeholder="Hae asemaa"
+                onChange={(event) => handleChange(event.target.value)}
+                value={query}
+            />
+            <div className="flex justify-center items-center w-full mr-5 border-y-2 border-r-2 my-2 h-11 rounded-r-md">
+                <FontAwesomeIcon
+                    icon={faClose}
+                    className={"text-gray-700 ml-8 hover:cursor-pointer"}
+                    size="lg"
+                    onClick={() => clearSearch()}
+                />
+            </div>
+        </div>
     );
 };
