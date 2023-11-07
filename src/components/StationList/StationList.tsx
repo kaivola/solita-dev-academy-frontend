9db623f402e325a6c87a08f4ef0e80aa6915ee93
vkaiv <ville.kaivola@gmail.com>
2023-11-07 18:03:08 +0200

    fixed StationList scrolling issue

--- src/components/StationList/StationList.tsx
+++ src/components/StationList/StationList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMap } from "react-leaflet";
 
 import { Station } from "@/lib/dev-academy-assignment";
@@ -14,12 +14,23 @@ type Props = {
     stations: Station[];
 };
 
+const stopPropagation = (event: WheelEvent) => event.stopPropagation();
+
 export const StationList = ({ stations }: Props) => {
     const [sortedStations, setSortedStations] = useState<Station[]>(stations);
     const [filteredStations, setFilteredStations] = useState<Station[]>(stations);
 
+    const listRef = useRef<HTMLDivElement>(null);
     const router = useRouter();
     const map = useMap();
+
+    useEffect(() => {
+        const current = listRef.current;
+        if (current) {
+            current.addEventListener("wheel", stopPropagation);
+            return () => current.removeEventListener("wheel", stopPropagation);
+        }
+    }, []);
 
     const handleMarkerClick = (station: Station) => {
         router.push("/" + "?station=" + station.id);
@@ -30,7 +41,7 @@ export const StationList = ({ stations }: Props) => {
         <div className="absolute top-0 bottom-0 left-0 z-[999] h-[90%] py-4 ml-12 my-auto bg-white rounded-3xl shadow-lg 3xl:w-[12.5%] min-w-fit flex flex-col text-base">
             <StationSearch stations={sortedStations} setStations={setFilteredStations} />
             <StationSort stations={stations} setStations={setSortedStations} />
-            <div className="h-full overflow-auto whitespace-nowrap">
+            <div className="h-full overflow-auto whitespace-nowrap" ref={listRef}>
                 {filteredStations.map((s) => (
                     <StationListItem key={s.id} name={s.name} onClickHandler={() => handleMarkerClick(s)} />
                 ))}
