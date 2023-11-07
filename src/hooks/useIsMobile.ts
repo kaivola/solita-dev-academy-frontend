75c90d46f13dd20509ed2e46f6531d48f15b2c91
vkaiv <ville.kaivola@gmail.com>
2023-11-07 19:17:53 +0200

    hook for getting the view width

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export const useIsMobile = () => {
+    const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return screenWidth && screenWidth < 768;
+};
