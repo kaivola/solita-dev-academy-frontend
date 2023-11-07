8039398179fabe3a0b1066bbef85857d306ec5c8
vkaiv <ville.kaivola@gmail.com>
2023-11-07 18:01:14 +0200

    added panning to selected station

--- src/components/StationList/StationList.tsx
+++ src/components/StationList/StationList.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useMap } from "react-leaflet";
 
 import { Station } from "@/lib/dev-academy-assignment";
 
@@ -16,13 +18,26 @@ export const StationList = ({ stations }: Props) => {
     const [sortedStations, setSortedStations] = useState<Station[]>(stations);
     const [filteredStations, setFilteredStations] = useState<Station[]>(stations);
 
+    const router = useRouter();
+    const map = useMap();
+
+    const handleMarkerClick = (station: Station) => {
+        router.push("/" + "?station=" + station.id);
+        map.flyTo([station.coordinateY, station.coordinateX], 14);
+    };
+
     return (
-        <div className="absolute top-0 bottom-0 left-0 z-[999] h-[90%] py-4 ml-12 my-auto bg-white rounded-3xl shadow-lg w-1/4 2xl:w-1/6 3xl:w-[12.5%] flex flex-col">
+        <div className="absolute top-0 bottom-0 left-0 z-[999] h-[90%] py-4 ml-12 my-auto bg-white rounded-3xl shadow-lg w-1/4 2xl:w-1/6 3xl:w-[12.5%] flex flex-col text-base">
             <StationSearch stations={sortedStations} setStations={setFilteredStations} />
             <StationSort stations={stations} setStations={setSortedStations} />
             <div className="h-full overflow-auto">
                 {filteredStations.map((s) => (
-                    <StationListItem key={s.id} id={s.id} name={s.name} />
+                    <StationListItem
+                        key={s.id}
+                        id={s.id}
+                        name={s.name}
+                        onClickHandler={() => handleMarkerClick(s)}
+                    />
                 ))}
             </div>
         </div>
--- src/components/StationList/StationListItem/StationListItem.tsx
+++ src/components/StationList/StationListItem/StationListItem.tsx
@@ -2,24 +2,18 @@
 
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRouter } from "next/navigation";
 
 type Props = {
     id: number;
     name: string;
+    onClickHandler: () => void;
 };
 
-export const StationListItem = ({ id, name }: Props) => {
-    const router = useRouter();
-
-    const handleClick = (id: number) => {
-        router.push("/" + "?station=" + id);
-    };
-
+export const StationListItem = ({ id, name, onClickHandler }: Props) => {
     return (
         <div className="px-8 border-b flex items-center justify-between hover:bg-slate-100">
             <div
-                onClick={() => handleClick(id)}
+                onClick={() => onClickHandler()}
                 className="rounded-full flex items-center justify-between w-full h-12 hover:cursor-pointer"
             >
                 <div className="text-black overflow-hidden">{name}</div>
