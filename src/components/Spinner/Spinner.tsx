e9ee8f9a67b0ddba718cf86fd9e60c2543af82d0
vkaiv <ville.kaivola@gmail.com>
2023-11-11 18:20:45 +0200

    added loading spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+    return (
+        <div className="w-full flex justify-center">
+            <div className="border-t-transparent border-solid animate-spin rounded-full border-emerald-300 border-[6px] h-14 w-14"></div>
+        </div>
+    );
+};
