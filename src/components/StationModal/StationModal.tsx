201d8a5d7d703dc189679a423ba162bd0325c15b
vkaiv <ville.kaivola@gmail.com>
2023-11-11 18:23:33 +0200

    added loading spinner to StationModal

--- src/components/StationModal/StationModal.tsx
+++ src/components/StationModal/StationModal.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 
 import InfoCard from "@/components/InfoCard";
 import Modal from "@/components/Modal";
+import Spinner from "@/components/Spinner";
 import { Station } from "@/lib/dev-academy-assignment";
 import { formatDistance, formatDuration } from "@/lib/formatStationStatistics";
 
@@ -14,8 +15,9 @@ import TopDestinationsTable from "./TopDestinationsTable";
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 export const StationModal = () => {
-    const router = useRouter();
     const [station, setStation] = useState<Station>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
     const params = useSearchParams();
     const stationId = params.get("station");
 
@@ -27,39 +29,47 @@ export const StationModal = () => {
     const changeStation = (id: number) => router.push("/" + "?station=" + id);
 
     useEffect(() => {
-        if (!stationId) return;
-        (async () => {
-            const res = await fetch(`${APP_URL}/api/stations/${stationId}`);
-            if (res.ok && res.status == 200) {
-                setStation(await res.json());
-            }
-        })();
+        if (stationId) {
+            (async () => {
+                const res = await fetch(`${APP_URL}/api/stations/${stationId}`);
+                if (res.ok && res.status == 200) {
+                    setStation(await res.json());
+                    setLoading(false);
+                }
+            })();
+        }
     }, [stationId]);
 
-    if (!station || !stationId) return null;
+    if (!stationId || (!station && !loading)) return null;
     return (
         <Modal handleClose={closeModal}>
-            <div className="text-3xl font-semibold">{station.name}</div>
-            <div className="text-lg">{station.address}</div>
-            <div className="pb-4">
-                <div className="my-4 grid lg:grid-cols-2 gap-4">
-                    <InfoCard title={"Journeys started"} value={station.numOfJourneysStarting} />
-                    <InfoCard title={"Journeys ended"} value={station.numOfJourneysEnding} />
-                    <InfoCard
-                        title={"Average distance"}
-                        value={formatDistance(station.averageDistanceOfJourneys)}
-                    />
-                    <InfoCard
-                        title={"Average duration"}
-                        value={formatDuration(station.averageDurationOfJourneys)}
-                    />
+            {loading || !station ? (
+                <Spinner />
+            ) : (
+                <div className="md:w-[40vw] 3xl:w-[35vw] max-w-5xl">
+                    <div className="text-3xl font-semibold">{station.name}</div>
+                    <div className="text-lg">{station.address}</div>
+                    <div className="pb-4">
+                        <div className="my-4 grid lg:grid-cols-2 gap-4">
+                            <InfoCard title={"Journeys started"} value={station.numOfJourneysStarting} />
+                            <InfoCard title={"Journeys ended"} value={station.numOfJourneysEnding} />
+                            <InfoCard
+                                title={"Average distance"}
+                                value={formatDistance(station.averageDistanceOfJourneys)}
+                            />
+                            <InfoCard
+                                title={"Average duration"}
+                                value={formatDuration(station.averageDurationOfJourneys)}
+                            />
+                        </div>
+                        <div className="text-xl py-1.5">Top destinations</div>
+                        <TopDestinationsTable
+                            destinations={station.topDestinations}
+                            changeStationHandler={(id: number) => changeStation(id)}
+                        />
+                    </div>
                 </div>
-                <div className="text-xl py-1.5">Top destinations</div>
-                <TopDestinationsTable
-                    destinations={station.topDestinations}
-                    changeStationHandler={(id: number) => changeStation(id)}
-                />
-            </div>
+            )}
         </Modal>
     );
 };
