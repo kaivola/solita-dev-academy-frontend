3929760023f12399f08460d83766639870e15577
vkaiv <ville.kaivola@gmail.com>
2023-11-11 11:29:41 +0200

    added top destinations to single station view

--- src/components/StationModal/StationModal.tsx
+++ src/components/StationModal/StationModal.tsx
@@ -9,6 +9,8 @@ import Modal from "@/components/Modal";
 import { Station } from "@/lib/dev-academy-assignment";
 import { formatDistance, formatDuration } from "@/lib/formatStationStatistics";
 
+import TopDestinationsTable from "./TopDestinationsTable";
+
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 export const StationModal = () => {
@@ -19,8 +21,10 @@ export const StationModal = () => {
 
     const closeModal = () => {
         setStation(undefined);
-        router.back();
+        router.push("/");
     };
+
+    const changeStation = (id: number) => router.push("/" + "?station=" + id);
 
     useEffect(() => {
         if (!stationId) return;
@@ -35,18 +39,25 @@ export const StationModal = () => {
     if (!station || !stationId) return null;
     return (
         <Modal handleClose={closeModal}>
-            <div className="text-3xl">{station.name}</div>
+            <div className="text-3xl font-semibold">{station.name}</div>
             <div className="text-lg">{station.address}</div>
-            <div className="mt-4 grid lg:grid-cols-2 gap-4">
-                <InfoCard title={"Journeys started from the station"} value={station.numOfJourneysStarting} />
-                <InfoCard title={"Journeys ended at the station"} value={station.numOfJourneysEnding} />
-                <InfoCard
-                    title={"Average distance of the started journeys"}
-                    value={formatDistance(station.averageDistanceOfJourneys)}
-                />
-                <InfoCard
-                    title={"Average duration of the started journeys"}
-                    value={formatDuration(station.averageDurationOfJourneys)}
+            <div className="pb-4">
+                <div className="my-4 grid lg:grid-cols-2 gap-4">
+                    <InfoCard title={"Journeys started"} value={station.numOfJourneysStarting} />
+                    <InfoCard title={"Journeys ended"} value={station.numOfJourneysEnding} />
+                    <InfoCard
+                        title={"Average distance"}
+                        value={formatDistance(station.averageDistanceOfJourneys)}
+                    />
+                    <InfoCard
+                        title={"Average duration"}
+                        value={formatDuration(station.averageDurationOfJourneys)}
+                    />
+                </div>
+                <div className="text-xl py-1.5">Top destinations</div>
+                <TopDestinationsTable
+                    destinations={station.topDestinations}
+                    changeStationHandler={(id: number) => changeStation(id)}
                 />
             </div>
         </Modal>
